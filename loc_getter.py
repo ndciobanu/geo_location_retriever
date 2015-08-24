75f18913f81420a764c674330f3eda646f52ac60
Nathan Ciobanu <nathanciobanu@gmail.com>
2015-08-23 19:40:27 -0700

    Finished writing main(), stubbed out get_geo()
    BUG=none
    TEST="loc_getter.py --help" and make sure there are no
         syntax errors
    
    Outlined the main() function and stubbed out the
    get_geo() function.
    
    Signed-off-by: Nathan Ciobanu <nathanciobanu@gmail.com>

--- loc_getter.py
+++ loc_getter.py
@@ -1,18 +1,41 @@
 import argparse
 
 
-def main(args):
-    None
 
-    
-    
+def get_geo(addr):
+    #TODO: implement this
+    m_geo_data = {}
+    return m_geo_data
+
+
+
+def main(args):
+    geo_data = {}
+    if args.address:
+        addr = args.address
+        plus_address = addr.replace(' ', '+')
+        geo_data = get_geo(plus_address)
+        print (addr + ' lat: ' + geo_data['lat'] + ' lng: '\
+                           + geo_data['lng'])
+
+    if args.addr_file:
+        with open(args.addr_file, 'r') as f:
+            addresses = f.readlines()
+            for addr in addresses:
+                plus_address = addr.replace(' ', '+')
+                geo_data = get_geo(plus_address)
+                print (addr + ' lat: ' + geo_data['lat'] + ' lng: '\
+                           + geo_data['lng'])
+
+
+ 
 if __name__ == "__main__":
     aparser = argparse.ArgumentParser(
                          description="Geo location "\
                          +"retriever arg parser")
     aparser.add_argument("--address", metavar="Street address",
                          help = "Enter a physical address")
-    aparser.add_argument("--file", metavar="List of addresses file",
+    aparser.add_argument("--addr_file", metavar="List of addresses file",
                          help = "Enter a file name with a list of "\
                          +"addresses, one per line")
     args = aparser.parse_args()
